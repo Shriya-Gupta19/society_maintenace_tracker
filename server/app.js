ec33eb640f18d51664c0dcdb7a8224d1b672df4c
Shriya-Gupta19 <shriyagpt1914@gmail.com>
2026-07-01 21:51:59 +0530

    Initial project setup with React, Express and MongoDB

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Society Maintenance Tracker API Running"
+    });
+});
+
+module.exports = app;
